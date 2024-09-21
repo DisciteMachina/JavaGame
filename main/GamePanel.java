b507aa0a9346da35c4d90484b4866dbd6bffc9fc
chris <chrishicks.w@gmail.com>
2024-09-20 19:12:15 -0500

    Cleaning up the code a bit

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -2,29 +2,31 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import entity.Player;
 
 public class GamePanel extends JPanel implements Runnable{
 
     // SCREEN SETTINGS
     final int originalTileSize = 16;
     final int scale = 3;
-    final int tileSize = originalTileSize * scale; // 48x48
+    public final int tileSize = originalTileSize * scale; // 48x48
 
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
 
-    final int screenWidth = tileSize * maxScreenCol; // 768
-    final int screenHeight = tileSize * maxScreenRow; // 576
+    public final int screenWidth = tileSize * maxScreenCol; // 768
+    public final int screenHeight = tileSize * maxScreenRow; // 576
 
     double fps = 60; //fps
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyH);
 
     // Character default position
     int playerX = 100;
     int playerY = 100;
-    int playerSpeed = 5;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -73,22 +75,7 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update() {
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        if (keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        if (keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }
-        if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
-
-        // Prevent player from moving out of bounds
-        playerX = Math.max(0, Math.min(playerX, screenWidth - tileSize));
-        playerY = Math.max(0, Math.min(playerY, screenHeight - tileSize));
+        player.update();
     }
 
 
@@ -96,8 +83,7 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.WHITE);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
